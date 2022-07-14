a02f06f4af3518d9a51be650282e6e3d2c10099f
Jiganov Andrew <68337015+AnderRester@users.noreply.github.com>
2022-07-14 17:39:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import mongoose from "mongoose";
+import Post from "./Post.js";
+import router from "./Router.js";
+
+const PORT = 15000;
+const DB_URL = `mongodb+srv://andrew:root@back-end-learning.iduwr.mongodb.net/?retryWrites=true&w=majority`
+
+const app = express();
+
+app.use(express.json());
+app.use('/api', router);
+
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log("Server started on port " + PORT));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+startApp(); // Connect to DB
+
+// app.listen(PORT, () => console.log("Server is Online " + PORT));
